f3b7b405e4297842ff3ca5596c6c1c3ad3f0cf36
Josue Rodriguez <josuerodriguez@Josues-MacBook-Pro.local>
2020-11-20 13:24:19 -0800

    • Creates very basic skeleton of what the About page is suppose to look like

---
+++ anime-archive/src/pages/About.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1></h1>
+      <div>
+        <h2>Anime Lovers</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deser
+        </p>
+      </div>
+      <div>
+        <h2>Meet the Developers</h2>
+        <div>
+          <div>
+            <div>Insert IMAGE HERE</div>
+            <div>
+              <div>
+                <h4>Josue Rodriguez</h4>
+                <h6>Software Engineer</h6>
+              </div>
+              <div>
+                <h8>Contact:</h8>
+                <div>INSERT ICONS HERE</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
