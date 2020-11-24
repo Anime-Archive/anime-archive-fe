56d44b345b032f3cf5de86fb183376f4deadebfd
Erik Rodriguez <erikrodriguez@Eriks-MBP.lan1>
2020-11-24 11:15:02 -0800

    • Creates logo component in components folder • Adds logo component to Home.js file

--- anime-archive/src/pages/Home.js
+++ anime-archive/src/pages/Home.js
@@ -1,9 +1,15 @@
+import Logo from "../components/logo/Logo";
+import SearchImg from "../assets/icons/searchIcon.png";
 import "../App.css";
 
 export default function Home() {
   return (
     <div>
-      <p>home.js</p>
+      <div>
+        <Logo />
+        {/* Wrap link or onClick to send to searchpage vvvvvv */}
+        <img src={SearchImg} alt="search icon" />
+      </div>
     </div>
   );
 }
