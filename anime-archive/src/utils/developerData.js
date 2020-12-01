adedd6129cebfcfb522edc90a522c233a5b4e918
Josue Rodriguez <josuerodriguez@Josues-MacBook-Pro.local>
2020-12-01 13:59:52 -0800

    • Creates developer data structure

---
+++ anime-archive/src/utils/developerData.js
@@ -0,0 +1,20 @@
+export const developerData = [
+  {
+    name: "Erik Rodriguez",
+    role: "Software Engineer",
+    media: {
+      portfolio: "https://erikrodriguez.me/",
+      linkedin: "https://www.linkedin.com/in/erikrodriguez-webdev/",
+      github: "https://github.com/ErikRodriguez-webdev",
+    },
+  },
+  {
+    name: "Josue Rodriguez",
+    role: "Software Engineer",
+    media: {
+      portfolio: "https://josuerodriguez.me/",
+      linkedin: "https://www.linkedin.com/in/maybejosue/",
+      github: "https://github.com/maybejosue",
+    },
+  },
+];
