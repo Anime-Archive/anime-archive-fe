741bce06835a9caa6d51fc0fbd66de50889a4452
Josue Rodriguez <josuerodriguez@Josues-MacBook-Pro.local>
2020-12-01 12:18:03 -0800

    Completes styling in order to make about us page look like original figma • imports icons and images of developers • completes html structure • implemented proper stlyes

--- anime-archive/src/pages/About.js
+++ anime-archive/src/pages/About.js
@@ -1,39 +1,61 @@
 import React from "react";
+import "./About.css";
+import Erik from "../assets/img/Erik.jpeg";
+import portfolio from "../assets/icons/portfolio.png";
+import github from "../assets/icons/github.png";
+import linkedin from "../assets/icons/linkedin.png";
 
 export default function About() {
   return (
-    <div>
-      <h1></h1>
-      <div>
-        <h2>Anime Lovers</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deser
-        </p>
-      </div>
-      <div>
-        <h2>Meet the Developers</h2>
-        <div>
-          <div>
-            <div>Insert IMAGE HERE</div>
-            <div>
-              <div>
-                <h4>Josue Rodriguez</h4>
-                <h6>Software Engineer</h6>
-              </div>
-              <div>
-                <h8>Contact:</h8>
-                <div>INSERT ICONS HERE</div>
+    <section id="about">
+      <div className="about">
+        <section className="sub-section">
+          <div className="anime-lovers">
+            <h5 className="subheading">Anime Lovers</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deser
+            </p>
+          </div>
+        </section>
+        <section className="sub-section">
+          <div className="meet-the-developers">
+            <h5 className="subheading developers">Meet the Developers</h5>
+            <div className="card">
+              <div className="card-content-conatiner">
+                <div className="developer-img-container">
+                  <img className="developer-img" src={Erik} />
+                </div>
+                <div className="content">
+                  <div className="dev-info">
+                    <p className="card-text">Josue Rodriguez</p>
+                    <p className="card-text">Software Engineer</p>
+                  </div>
+                  <div className="about-contact-info">
+                    <p className="contact">Contact:</p>
+                    <div className="about-icon-container">
+                      <div className="icon-container">
+                        <img src={portfolio} />
+                      </div>
+                      <div className="icon-container">
+                        <img src={linkedin} />
+                      </div>
+                      <div className="icon-container">
+                        <img src={github} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </section>
       </div>
-    </div>
+    </section>
   );
 }
