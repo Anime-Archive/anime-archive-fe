4fe7f44cb2767ed91908301dff3e7ab7e0c4561c
Josue Rodriguez <josuerodriguez@Josues-MacBook-Pro.local>
2020-12-01 17:56:29 -0800

    • Updates developerData data structure in order to make developer social media links dynamic • Refactors About.js Page by breaking out the cards into a different components - more dynamic • Breaks out CSS and places themm into their respective components

---
+++ anime-archive/src/pages/About/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./About.css";
+import { DevCard } from "../../components/devCard/DevCard.js";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="about">
+        <section className="sub-section">
+          <div className="anime-lovers">
+            <h5 className="subheading">Anime Lovers</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deser
+            </p>
+          </div>
+        </section>
+        <section className="sub-section">
+          <div className="meet-the-developers">
+            <h5 className="subheading developers">Meet the Developers</h5>
+            {/* Displaying cards for each developer who worked on application */}
+            <DevCard />
+          </div>
+        </section>
+      </div>
+    </section>
+  );
+}
--- anime-archive/src/utils/developerData.js
+++ anime-archive/src/utils/developerData.js
@@ -2,26 +2,63 @@
 import Erik from "../assets/img/Erik.jpeg";
 import Josue from "../assets/img/Josue.jpeg";
 
+// Icons
+import portfolio from "../assets/icons/portfolio.png";
+import github from "../assets/icons/github.png";
+import linkedin from "../assets/icons/linkedin.png";
+
 // Developer data structure
 export const developerData = [
   {
+    id: 1,
     name: "Erik Rodriguez",
     img: Erik,
     role: "Software Engineer",
-    media: {
-      portfolio: "https://erikrodriguez.me/",
-      linkedin: "https://www.linkedin.com/in/erikrodriguez-webdev/",
-      github: "https://github.com/ErikRodriguez-webdev",
-    },
+    media: [
+      {
+        id: 1,
+        name: "Portfolio",
+        link: "https://erikrodriguez.me/",
+        icon: portfolio,
+      },
+      {
+        id: 2,
+        name: "LinkedIn",
+        link: "https://www.linkedin.com/in/erikrodriguez-webdev/",
+        icon: linkedin,
+      },
+      {
+        id: 3,
+        name: "Github",
+        link: "https://github.com/ErikRodriguez-webdev",
+        icon: github,
+      },
+    ],
   },
   {
+    id: 2,
     name: "Josue Rodriguez",
     img: Josue,
     role: "Software Engineer",
-    media: {
-      portfolio: "https://josuerodriguez.me/",
-      linkedin: "https://www.linkedin.com/in/maybejosue/",
-      github: "https://github.com/maybejosue",
-    },
+    media: [
+      {
+        id: 1,
+        name: "Portfolio",
+        link: "https://josuerodriguez.me/",
+        icon: portfolio,
+      },
+      {
+        id: 2,
+        name: "LinkedIn",
+        link: "https://www.linkedin.com/in/maybejosue/",
+        icon: linkedin,
+      },
+      {
+        id: 3,
+        name: "Github",
+        link: "https://github.com/maybejosue",
+        icon: github,
+      },
+    ],
   },
 ];
