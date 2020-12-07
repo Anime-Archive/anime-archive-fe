7c8493c8eb3ded54e67abe01cba8f3091c2fd0c1
Josue Rodriguez <josuerodriguez@Josues-MBP.lan1>
2020-12-07 15:54:17 -0800

    • Creates function that places black indicator under the users current filter options in the carousel

--- anime-archive/src/components/filterCard/FilterCard.js
+++ anime-archive/src/components/filterCard/FilterCard.js
@@ -1,9 +1,30 @@
 import "./FilterCard.css";
 
 const FilterCard = (props) => {
+  // Places black indicator on current filter in carousel
+  const currentFilter = (filterId) => {
+    const filter = document.querySelector(`.filter-${filterId}`);
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+    filter.appendChild(dot);
+  };
+
+  window.onload = () => {
+    currentFilter(1);
+  };
+
   return (
     <div className="filterCard">
-      <p>{props.data.name}</p>
+      <div
+        className={`filterContent filter-${props.data.id}`}
+        onClick={() => {
+          const oldFilter = document.querySelector(`.dot`);
+          oldFilter.remove();
+          currentFilter(props.data.id);
+        }}
+      >
+        <p>{props.data.name}</p>
+      </div>
     </div>
   );
 };
