7cc52d367e816859df8f8578dbb65e93501d2376
Erik Rodriguez <erikrodriguez@Eriks-MBP.lan1>
2020-12-07 14:11:36 -0800

    • Adds new route to app.js for search page • Creates new page folder in pages Search with js and css file inside

--- anime-archive/src/App.js
+++ anime-archive/src/App.js
@@ -1,6 +1,7 @@
 import { Route } from "react-router-dom";
 import Home from "./pages/Home/Home.js";
 import About from "./pages/About/About.js";
+import Search from "./pages/Search/Search.js";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,9 @@ function App() {
       </Route>
       <Route path="/about">
         <About />
+      </Route>
+      <Route path="/search">
+        <Search />
       </Route>
     </div>
   );
