ecc5cabf7a1abbaf4838b600e988ef1f3d0cb064
Erik Rodriguez <erikrodriguez@Eriks-MacBook-Pro.local>
2020-12-08 10:36:50 -0800

    • Adds cards with dummy data into search.js • Adds load more button at bottom of page

--- anime-archive/src/pages/Search/Search.js
+++ anime-archive/src/pages/Search/Search.js
@@ -1,4 +1,6 @@
 import Logo from "../../components/logo/Logo.js";
+import AnimeCard from "../../components/animeCard/AnimeCard.js";
+import { animeData } from "../../utils/animeData.js";
 import SearchIcon from "../../assets/icons/searchIcon.png";
 import FilterIcon from "../../assets/icons/filterIcon.png";
 import "./Search.css";
@@ -23,6 +25,14 @@ export default function Search() {
         <div>
           <img src={FilterIcon} alt="filter" />
         </div>
+      </div>
+
+      {animeData.map((data) => (
+        <AnimeCard key={data.id} data={data} />
+      ))}
+
+      <div className="load">
+        <button>Load More</button>
       </div>
     </div>
   );
