4f062af9476453e3bb98ea2ac36281b7782b4d4b
Erik Rodriguez <erikrodriguez@Eriks-MacBook-Pro.local>
2020-12-08 13:22:37 -0800

    • Adds handler and submit functions to search.js for search functionality • Adds comments for readability to search.js

--- anime-archive/src/pages/Search/Search.js
+++ anime-archive/src/pages/Search/Search.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Logo from "../../components/logo/Logo.js";
 import AnimeCard from "../../components/animeCard/AnimeCard.js";
 import { animeData } from "../../utils/animeData.js";
@@ -6,6 +7,20 @@ import FilterIcon from "../../assets/icons/filterIcon.png";
 import "./Search.css";
 
 export default function Search() {
+  // User search input
+  const [searchText, setSearchText] = useState("");
+
+  // Grabs user text from input field and stores in searchText above
+  function searchHandler(event) {
+    setSearchText(event.target.value);
+  }
+
+  // User search text via query to api returns data result
+  function searchSubmit(event) {
+    event.preventDefault();
+    console.log(searchText);
+  }
+
   return (
     <div>
       <header>
@@ -15,20 +30,25 @@ export default function Search() {
       <div className="searchSection">
         <div className="searchBar">
           <img src={SearchIcon} alt="search" />
-          <input
-            type="text"
-            name="search"
-            autoComplete="off"
-            placeholder="Ex: One Piece"
-          />
+          <form onSubmit={(event) => searchSubmit(event)}>
+            <input
+              type="text"
+              name="search"
+              value={searchText}
+              autoComplete="off"
+              onChange={(event) => searchHandler(event)}
+              placeholder="Ex: One Piece"
+            />
+          </form>
         </div>
         <div>
           <img src={FilterIcon} alt="filter" />
         </div>
       </div>
 
-      {animeData.map((data) => (
-        <AnimeCard key={data.id} data={data} />
+      {/* Api search results turned into anime cards here */}
+      {animeData.map((result) => (
+        <AnimeCard key={result.id} data={result} />
       ))}
 
       <div className="load">
