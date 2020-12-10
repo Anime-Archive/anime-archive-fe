e8585ba0cb80cd8a468878dddbfe9414f57a07a5
Erik Rodriguez <erikrodriguez@Eriks-MacBook-Pro.local>
2020-12-09 19:16:57 -0800

    • Adds new dependency react-scroll • Replaces on click for backToTop component for react scroll transition

--- anime-archive/src/components/backToTop/BackToTop.js
+++ anime-archive/src/components/backToTop/BackToTop.js
@@ -1,9 +1,18 @@
+import Scroll from "react-scroll";
 import UpIcon from "../../assets/icons/upIcon.png";
 import "./BackToTop.css";
 
 const BackToTop = () => {
   return (
-    <div className="scrollToTop" onClick={() => window.scrollTo(0, 0)}>
+    <div
+      className="scrollToTop"
+      onClick={() =>
+        Scroll.animateScroll.scrollToTop({
+          duration: 500,
+          smooth: true,
+        })
+      }
+    >
       <img src={UpIcon} alt="up" />
     </div>
   );
