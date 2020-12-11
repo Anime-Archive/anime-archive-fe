fe1da8f0a7b8bbe540eb1c24744f3c1bf0ec047b
Josue Rodriguez <josuerodriguez@Josues-MacBook-Pro.local>
2020-12-10 17:07:08 -0800

    • Fixes issue where if the user started at the homepage, traveled to another page, and cameback to the homepage, there would be no default filter

--- anime-archive/src/components/filterCard/FilterCard.js
+++ anime-archive/src/components/filterCard/FilterCard.js
@@ -1,26 +1,12 @@
 import "./FilterCard.css";
 
 const FilterCard = (props) => {
-  // Places black indicator on current filter in carousel
-  const currentFilter = (filterId) => {
-    const filter = document.querySelector(`.filter-${filterId}`);
-    const dot = document.createElement("div");
-    dot.classList.add("dot");
-    filter.appendChild(dot);
-  };
-
-  window.onload = () => {
-    currentFilter(1);
-  };
-
   return (
     <div className="filterCard">
       <div
         className={`filterContent filter-${props.data.id}`}
         onClick={() => {
-          const oldFilter = document.querySelector(`.dot`);
-          oldFilter.remove();
-          currentFilter(props.data.id);
+          props.currentFilter(props.data.id);
         }}
       >
         <p>{props.data.name}</p>
