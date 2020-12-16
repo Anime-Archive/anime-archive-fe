899b023450ae9c9ac1a0165bad6ff2f5a4179984
Erik Rodriguez <perhapserik@gmail.com>
2020-12-16 11:45:00 -0800

    • Adds more dummyData for characters page

--- anime-archive/src/utils/closerlookData.js
+++ anime-archive/src/utils/closerlookData.js
@@ -33,128 +33,278 @@ export const closerlookData = [
     characters: {
       nodes: [
         {
+          id: 1,
           name: {
             full: "Spike Spiegel",
+            native: "スパイク・スピーゲル",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b1-ChxaldmieFlQ.png",
           },
         },
         {
+          id: 4,
           name: {
             full: "Ein",
+            native: "アイン",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/4.jpg",
           },
         },
         {
+          id: 2,
           name: {
             full: "Faye Valentine",
+            native: "フェイ・バレンタイン",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b2-0Iszg6Izgt4p.png",
           },
         },
         {
+          id: 23687,
           name: {
             full: "Rhint Celonias",
+            native: "リンド",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b23687-uzV1PzEyejhW.png",
           },
         },
         {
+          id: 2734,
           name: {
             full: "Vicious",
+            native: "ビシャス",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b2734-aglO8RKNVxnn.jpg",
           },
         },
         {
+          id: 85271,
           name: {
             full: "Stella Bonnaro",
+            native: "ステラ・ボナーロ",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b85271-l06jfkIpYFG2.jpg",
           },
         },
         {
+          id: 83271,
           name: {
             full: "MPU",
+            native: "んぴゅー",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/83271.jpg",
           },
         },
         {
+          id: 15713,
           name: {
             full: "Coffee",
+            native: "コフィ",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b15713-UBG3R1CYJkYG.png",
           },
         },
         {
+          id: 13249,
           name: {
             full: "Afedersiniz Hesap Lutfen",
+            native: "アップルデリー・シニズ・ヘサップ・リュトフェン",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b13249-0XZFxU3461Sv.png",
           },
         },
         {
+          id: 23740,
           name: {
             full: "Andy Von de Oniyate",
+            native: "アンディ・フォン・デ・オニアテ",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b23740-NCE7b9okKmeI.png",
           },
         },
         {
+          id: 6694,
           name: {
             full: "Judy",
+            native: "ジュディ",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b6694-y0PmKzrcVa7A.png",
           },
         },
         {
+          id: 35870,
           name: {
             full: "Shin",
+            native: "シン",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b35870-pw3TfWPtAoxJ.jpg",
           },
         },
         {
+          id: 23686,
           name: {
             full: "Roco Bonnaro",
+            native: "ロコ",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b23686-kI8TJoYQ4G5v.png",
           },
         },
         {
+          id: 2736,
           name: {
             full: "Grencia Mars Elijah Guo Eckener",
+            native: "グレン",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b2736-0Eoluq9UxXu4.png",
           },
         },
         {
+          id: 2735,
           name: {
             full: "Julia",
+            native: "ジュリア",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b2735-0NRiXHK4PSWs.png",
           },
         },
         {
+          id: 19119,
           name: {
             full: "Wen",
+            native: "ウェン",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b19119-dTpS5sAhlTD2.jpg",
           },
         },
         {
+          id: 29313,
           name: {
             full: "Mad Pierrot",
+            native: "東風",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b29313-F2xCYvnsemie.png",
           },
         },
         {
+          id: 3,
           name: {
             full: "Jet Black",
+            native: "ジェット・ブラック",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b3-JjH9Si9UM1NZ.png",
           },
         },
         {
+          id: 16,
           name: {
             full: "Edward Wong Hau Pepelu Tivrusky IV",
+            native: "エドワード・ウォン・ハウ・ペペル・チブルスキー4世",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b16-5Bqas3dviPPk.png",
           },
         },
         {
+          id: 19118,
           name: {
             full: "Laughing Bull",
+            native: "ブル",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b19118-5xDFbLvjIeLp.png",
           },
         },
         {
+          id: 42425,
           name: {
             full: "Fad",
+            native: null,
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b42425-rCfLBtVjcLIU.jpg",
           },
         },
         {
+          id: 36364,
           name: {
             full: "Meifa Puzi",
+            native: "メイファ",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b36364-jGuXkkNyIkl3.png",
           },
         },
         {
+          id: 29559,
           name: {
             full: "Abdul Hakim",
+            native: "ハキム",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b29559-qOTqO50T3EUx.png",
           },
         },
         {
+          id: 18441,
           name: {
             full: "Dr. Londez",
+            native: "ロンデス",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b18441-o4hphgnOerjz.png",
           },
         },
         {
+          id: 37186,
           name: {
             full: "Miles",
+            native: "マイルズ",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b37186-ajy73hedX5lZ.png",
           },
         },
       ],
