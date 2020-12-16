f7f739d06816bc84fbf994db94fec1a6c3fee7ff
Josue Rodriguez <maybejosue@gmail.com>
2020-12-16 09:14:19 -0800

    • Refactors section.js to dynamically house and run apiData • Updates sectionData data structure

--- anime-archive/src/components/section/Section.js
+++ anime-archive/src/components/section/Section.js
@@ -2,6 +2,9 @@ import AnimeCard from "../../components/animeCard/AnimeCard.js";
 import "./Section.css";
 
 const Section = (props) => {
+  const data = props.apiData;
+  console.log(props);
+  console.log(data[props.data.extension], "props in section");
   return (
     <section>
       <div className="sectionHeader">
@@ -9,9 +12,8 @@ const Section = (props) => {
         {/* Wrap view more with dynamic link to push to searchpage based on section user wants to view more in link */}
         <p>view more</p>
       </div>
-
       {/* With query we can pull 4 cards and cycle through dynamically */}
-      {props.data.query.map((item) => (
+      {data[props.data.extension].map((item) => (
         <AnimeCard key={item.id} data={item} />
       ))}
     </section>
--- anime-archive/src/utils/sectionData.js
+++ anime-archive/src/utils/sectionData.js
@@ -1,14 +1,12 @@
-import { animeData } from "./animeData";
-
 export const sectionData = [
   {
     id: 1,
-    sectionNames: "Recommended",
-    query: animeData,
+    sectionNames: "Upcoming",
+    extension: "upcoming",
   },
   {
     id: 2,
     sectionNames: "Airing",
-    query: animeData,
+    extension: "airing",
   },
 ];
