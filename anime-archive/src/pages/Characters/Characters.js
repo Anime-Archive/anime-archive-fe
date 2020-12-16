ec4ec001067cd31913808dcbeed1437258774527
Erik Rodriguez <perhapserik@gmail.com>
2020-12-16 14:50:18 -0800

    • Adds banner, title and floating card to character.js with styles

--- anime-archive/src/pages/Characters/Characters.js
+++ anime-archive/src/pages/Characters/Characters.js
@@ -1,9 +1,28 @@
+import { closerlookData } from "../../utils/closerlookData";
+import BackToTop from "../../components/backToTop/BackToTop.js";
+import { Footer } from "../../components/footer/Footer.js";
 import "./Characters.css";
 
 export default function Characters() {
+  const data = closerlookData[0];
   return (
     <div>
-      <p>in characters</p>
+      {/* Refactor Title and Banner  from Closerlook and Characters */}
+      <div className="charactersBg">
+        <div>
+          <img src={data.bannerImage} alt="anime banner" />
+        </div>
+        <div className="charactersHeroText">
+          <h3>{data.title.english}</h3>
+        </div>
+      </div>
+
+      <BackToTop />
+
+      {/* Top floating data card */}
+      <div className="charactersFloat"></div>
+
+      <Footer />
     </div>
   );
 }
