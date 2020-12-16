18f3e4b4dd9bec76710bc0e2e510161ef4f950f3
Erik Rodriguez <perhapserik@gmail.com>
2020-12-16 15:20:40 -0800

    • Adds dummyData to build character cards for characters page

---
+++ anime-archive/src/utils/charactersData.js
@@ -0,0 +1,46 @@
+export const characterData = [
+  {
+    title: {
+      english: "Cowboy Bebop",
+    },
+    bannerImage:
+      "https://s4.anilist.co/file/anilistcdn/media/anime/banner/1-T3PJUjFJyRwg.jpg",
+    characters: {
+      nodes: [
+        {
+          id: 1,
+          name: {
+            full: "Spike Spiegel",
+            native: "スパイク・スピーゲル",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b1-ChxaldmieFlQ.png",
+          },
+        },
+        {
+          id: 4,
+          name: {
+            full: "Ein",
+            native: "アイン",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/4.jpg",
+          },
+        },
+        {
+          id: 2,
+          name: {
+            full: "Faye Valentine",
+            native: "フェイ・バレンタイン",
+          },
+          image: {
+            large:
+              "https://s4.anilist.co/file/anilistcdn/character/large/b2-0Iszg6Izgt4p.png",
+          },
+        },
+      ],
+    },
+  },
+];
