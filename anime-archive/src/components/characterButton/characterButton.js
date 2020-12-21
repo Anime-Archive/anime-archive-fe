c282d3f19c3b6aa1427553bbb3021db0f7a5f732
Josue Rodriguez <maybejosue@gmail.com>
2020-12-20 17:25:06 -0800

    • Adds logic to character aliases - considers if specific character doesnt have any aliases • Fixes issue where last card in character carousel is cut off

--- anime-archive/src/components/characterButton/characterButton.js
+++ anime-archive/src/components/characterButton/characterButton.js
@@ -6,21 +6,27 @@ const characterButton = (props) => {
   return (
     <div className="cardContainer">
       {props.data.map((character) => (
-        <div className="characterCarouselCard" key={character.id}>
-          <div className="characterCarouselImageContainer">
-            <img src={character.image.large} alt="anime character" />
-          </div>
-          <div className="characterNames">
-            {/* Checks if character has both first and last name, if not display the one that does exist */}
-            <h4>
-              {character.name.last && character.name.first
-                ? `${character.name.last} ${character.name.first}`
-                : character.name.last
-                ? character.name.last
-                : character.name.first}
-            </h4>
-            <h4>Aliases:</h4>
-            <p>{character.name.alternative.join(", ")}</p>
+        <div className="characterCardPadding" key={character.id}>
+          <div className="characterCarouselCard">
+            <div className="characterCarouselImageContainer">
+              <img src={character.image.large} alt="anime character" />
+            </div>
+            <div className="characterNames">
+              {/* Checks if character has both first and last name, if not display the one that does exist */}
+              <h4>
+                {character.name.last && character.name.first
+                  ? `${character.name.last} ${character.name.first}`
+                  : character.name.last
+                  ? character.name.last
+                  : character.name.first}
+              </h4>
+              <h4>Aliases:</h4>
+              <p>
+                {character.name.alternative.length > 0
+                  ? character.name.alternative.join(", ")
+                  : "Character has no alias"}
+              </p>
+            </div>
           </div>
         </div>
       ))}
