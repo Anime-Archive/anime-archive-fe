74d5e1e6ae5ab7e6054a33deb3b6fe2dfdac83a6
Erik Rodriguez <perhapserik@gmail.com>
2020-12-22 10:15:49 -0800

    • Adds useEffect useState and useParams as imports to characters page

--- anime-archive/src/pages/Characters/Characters.js
+++ anime-archive/src/pages/Characters/Characters.js
@@ -1,12 +1,28 @@
-import CharacterCard from "../../components/characterCard/characterCard.js";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { characterData } from "../../utils/charactersData.js";
+// Components
+import CharacterCard from "../../components/characterCard/characterCard.js";
 import BackToTop from "../../components/backToTop/BackToTop.js";
 import { Footer } from "../../components/footer/Footer.js";
 import Banner from "../../components/banner/Banner.js";
+// Styling
 import "./Characters.css";
 
 export default function Characters() {
   const data = characterData[0];
+
+  // Holds data for character cards
+  const [charactersData, setCharactersData] = useState(null);
+
+  // Grabs anime id from url
+  const { id } = useParams();
+
+  // Data call to api for all characters in anime
+  useEffect(() => {
+    console.log(id);
+  }, []);
+
   return (
     <div>
       {/* Title and Banner */}
