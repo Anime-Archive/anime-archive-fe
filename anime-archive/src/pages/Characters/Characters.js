f64d78c7780214a8116d90744467b80a4b28debf
Erik Rodriguez <perhapserik@gmail.com>
2020-12-22 11:02:33 -0800

    • Adds axios import to characters.js Adds api call to characters.js using query and passing params id as variable

--- anime-archive/src/pages/Characters/Characters.js
+++ anime-archive/src/pages/Characters/Characters.js
@@ -1,13 +1,17 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 import { characterData } from "../../utils/charactersData.js";
 // Components
 import CharacterCard from "../../components/characterCard/characterCard.js";
 import BackToTop from "../../components/backToTop/BackToTop.js";
 import { Footer } from "../../components/footer/Footer.js";
 import Banner from "../../components/banner/Banner.js";
+import { Loader } from "../../components/loader/Loader.js";
 // Styling
 import "./Characters.css";
+// Graphql
+import { fetchCharacters } from "../../graphql/index.js";
 
 export default function Characters() {
   const data = characterData[0];
@@ -18,23 +22,51 @@ export default function Characters() {
   // Grabs anime id from url
   const { id } = useParams();
 
+  // Loader
+  const [charactersLoading, setCharactersLoading] = useState(false);
+
   // Data call to api for all characters in anime
   useEffect(() => {
-    console.log(id);
+    setCharactersLoading(true);
+    axios
+      .post("https://graphql.anilist.co", {
+        query: fetchCharacters,
+        variables: { id: id },
+      })
+      .then(function (response) {
+        console.log(response);
+        setCharactersData(response);
+        setCharactersLoading(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+        setCharactersLoading(false);
+      });
   }, []);
 
   return (
     <div>
       {/* Title and Banner */}
-      <Banner title={data.title.english} bannerImage={data.bannerImage} />
+      {!charactersData || charactersLoading ? (
+        <Loader />
+      ) : (
+        <Banner
+          title={charactersData.data.data.Media.title.english}
+          bannerImage={charactersData.data.data.Media.bannerImage}
+        />
+      )}
 
       <BackToTop />
 
       {/* Top floating data card */}
       <div className="charactersFloat">
-        {data.characters.nodes.map((character) => (
-          <CharacterCard key={character.id} data={character} />
-        ))}
+        {!charactersData || charactersLoading ? (
+          <Loader />
+        ) : (
+          charactersData.data.data.Media.characters.nodes.map((character) => (
+            <CharacterCard key={character.id} data={character} />
+          ))
+        )}
       </div>
 
       <Footer />
