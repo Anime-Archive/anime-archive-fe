faf17cb1c80ba96365780d3dcc59e6effd0b6dfc
Erik Rodriguez <perhapserik@gmail.com>
2020-12-21 17:01:04 -0800

    • Adds hero banner as a reusable component made to figma reference

--- anime-archive/src/components/banner/Banner.js
+++ anime-archive/src/components/banner/Banner.js
@@ -1,9 +1,15 @@
 import "./Banner.css";
 
-const Banner = () => {
+const Banner = (props) => {
+  // Pass in an image and title for banner component
   return (
-    <div>
-      <p>inside Banner component</p>
+    <div className="bannerBg">
+      <div>
+        <img src={props.bannerImage} alt="anime banner" />
+      </div>
+      <div className="bannerText">
+        <h3>{props.title}</h3>
+      </div>
     </div>
   );
 };
