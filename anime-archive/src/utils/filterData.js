94c4ab37c52ed22c12acb3528211f677fb62c2bc
Erik Rodriguez <perhapserik@gmail.com>
2020-12-28 15:04:37 -0800

    • Creates data structure for dry code

---
+++ anime-archive/src/utils/filterData.js
@@ -0,0 +1,131 @@
+export const filterData = [
+  {
+    id: 1,
+    title: "Sort",
+    name: "sort",
+    options: [
+      {
+        id: 1,
+        title: "Alphabetical",
+        value: "TITLE_ENGLISH",
+      },
+      {
+        id: 2,
+        title: "Favorites",
+        value: "FAVOURITES_DESC",
+      },
+      {
+        id: 3,
+        title: "Popular",
+        value: "POPULARITY_DESC",
+      },
+      {
+        id: 4,
+        title: "Score",
+        value: "SCORING_DESC",
+      },
+      {
+        id: 5,
+        title: "Trending",
+        value: "TRENDING_DESC",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Genre",
+    name: "genre",
+    options: [
+      {
+        id: 1,
+        title: "Action",
+        value: "action",
+      },
+      {
+        id: 2,
+        title: "Adventure",
+        value: "adventure",
+      },
+      {
+        id: 3,
+        title: "Comedy",
+        value: "comedy",
+      },
+      {
+        id: 4,
+        title: "Drama",
+        value: "drama",
+      },
+      {
+        id: 5,
+        title: "Ecchi",
+        value: "ecchi",
+      },
+      {
+        id: 6,
+        title: "Fantasy",
+        value: "fantasy",
+      },
+      {
+        id: 7,
+        title: "Horror",
+        value: "horror",
+      },
+      {
+        id: 8,
+        title: "Mahou Shoujo",
+        value: "mahou shoujo",
+      },
+      {
+        id: 9,
+        title: "Mecha",
+        value: "mecha",
+      },
+      {
+        id: 10,
+        title: "Music",
+        value: "music",
+      },
+      {
+        id: 11,
+        title: "Mystery",
+        value: "mystery",
+      },
+      {
+        id: 12,
+        title: "Psychological",
+        value: "psychological",
+      },
+      {
+        id: 13,
+        title: "Romance",
+        value: "romance",
+      },
+      {
+        id: 14,
+        title: "Sci-Fi",
+        value: "sci-fi",
+      },
+      {
+        id: 15,
+        title: "Slice of Life",
+        value: "slice of life",
+      },
+      {
+        id: 16,
+        title: "Sports",
+        value: "sports",
+      },
+      {
+        id: 17,
+        title: "Supernatural",
+        value: "supernatural",
+      },
+      {
+        id: 18,
+        title: "Thriller",
+        value: "thriller",
+      },
+    ],
+  },
+];
