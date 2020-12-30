e20cae95eb37089bb14cfe442a1686976e246268
Josue Rodriguez <maybejosue@gmail.com>
2020-12-30 13:18:52 -0800

    • Adds comments to buildQueryString function

--- anime-archive/src/utils/buildQueryString.js
+++ anime-archive/src/utils/buildQueryString.js
@@ -3,8 +3,10 @@ export const buildQueryString = (obj) => {
   let string = "?";
   for (const filter in obj) {
     if (obj[filter]) {
+      // if filter is an object and length is 1 make a query string
       if (typeof obj[filter] === "object" && obj[filter].length === 1) {
         string += `${filter}=${obj[filter][0]}&`;
+        // if its type string add it to the string
       } else if (typeof obj[filter] === "string") {
         string += `${filter}=${obj[filter]}&`;
       }
