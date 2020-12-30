ea20293b7d22c448edcab4efcef51e65ecc5f426
Erik Rodriguez <perhapserik@gmail.com>
2020-12-30 11:11:36 -0800

    • Adds new default image to assets • Adds ternary incase anime has no banner to instead show a default image

--- anime-archive/src/components/banner/Banner.js
+++ anime-archive/src/components/banner/Banner.js
@@ -1,11 +1,17 @@
+import DefaultBanner from "../../assets/img/defaultBanner.png";
 import "./Banner.css";
 
 const Banner = (props) => {
   // Pass in an image and title for banner component
+  console.log(props.bannerImage);
   return (
     <div className="bannerBg">
       <div>
-        <img src={props.bannerImage} alt="anime banner" />
+        {props.bannerImage ? (
+          <img src={props.bannerImage} alt="anime banner" />
+        ) : (
+          <img src={DefaultBanner} alt="default anime banner" />
+        )}
       </div>
       <div className="bannerText">
         <h3>{props.title}</h3>
