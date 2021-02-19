7dcfac5d39d6e47019140ba6c7105e9028e63c49
Josue Rodriguez <maybejosue@gmail.com>
2021-02-19 14:36:01 -0800

    Initializes apollo client

--- anime-archive/src/index.js
+++ anime-archive/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import {ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+
+const client = new ApolloClient ({
+  uri: "https://graphql.anilist.co",
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <Router>
       <App />
     </Router>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
